80e1e50b15c03f88a78991135bcabc5693c35e58
vishalsoni008 <vishalmundawra@gmail.com>
2022-08-15 14:18:39 +0530

    Initial commit

---
+++ mvp-backend/src/main/java/com/mvpbackend/MvpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mvpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpBackendApplication.class, args);
+	}
+
+}
---
+++ mvp-backend/src/test/java/com/mvpbackend/MvpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
